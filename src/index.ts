136d57ce2b4dd8588f3c4b22bcf943e89f727801
WHenderson <whenderson.github+smartgit@gmail.com>
2020-02-24 14:34:00 +1100

    Packing for trial release

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { AbortablePromiseLike } from "./AbortablePromiseLike";
+import { CancellablePromiseLike } from "./CancellablePromiseLike";
+import { AbortApiInternal, AbortApi } from "./AbortApi";
+import { Abortable } from './Abortable';
+import { Timeout } from "./Timeout";
+import { Process } from "./Process";
