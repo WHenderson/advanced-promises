f0b537a919ba084376ac9d1e07f858e4aace8277
WHenderson <whenderson.github+smartgit@gmail.com>
2020-02-24 23:46:46 +1100

    export library interfaces

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,13 @@ import { AbortApiInternal, AbortApi } from './AbortApi';
 import { Abortable } from './Abortable';
 import { Timeout } from './Timeout';
 import { Process } from './Process';
+
+export {
+    AbortablePromiseLike,
+    CancellablePromiseLike,
+    AbortApiInternal,
+    AbortApi,
+    Abortable,
+    Timeout,
+    Process
+}
